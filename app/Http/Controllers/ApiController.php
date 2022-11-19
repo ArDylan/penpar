a41f0f9e7472aae8f8b45a0f3c7bcb9f675e75f5
Hengki Agung Prayoga <xeon699@gmail.com>
2022-11-19 11:05:33 +0800

    upload file

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,10 +9,14 @@ class ApiController extends Controller
 {
     public function createImage(Request $request)
     {
+        $nameFile = $request->point_id . "-" . $request->image->getClientOriginalName();
+        $request->image->storeAs(
+            'public/image', $nameFile
+        );
         PointImage::create([
             "point_id" => $request->point_id,
             "note" => $request->note,
-            "images" => "https://cdn1-production-images-kly.akamaized.net/Az6f4vox01osWGdRHyQnSt3hlhU=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3716202/original/023989000_1639554358-0000465749.jpg"
+            "images" => $nameFile
         ]);
         
         return true;
